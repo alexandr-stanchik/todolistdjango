9fd3c189d0a9f2c2c50936deb245e2b344d00fc2
Alexandr-Stanchik <lex023ott@mail.ru>
2023-03-11 20:11:16 +0300

    Added the Tusk model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date_start = models.DateTimeField()
+    date_start = models.DateTimeField()
+    is_done = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
